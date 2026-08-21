6de8319d38d5631204c4c0a0fa2f9dc8aa3eb86e
Fernán González Ibáñez <62150229+nanrefGI@users.noreply.github.com>
2026-08-21 12:44:55 +0200

    Update wordle.js

--- assets/js/wordle.js
+++ assets/js/wordle.js
@@ -21,7 +21,7 @@ var WordleES = (function () {
   var TECLADO = [
     ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
     ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'ñ'],
-    ['↵', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '⌫']
+    ['✓', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '⌫']
   ];
 
   var CSS = [
